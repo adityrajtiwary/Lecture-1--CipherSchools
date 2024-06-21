8a14a3c075ba529d9489795d8d47e2424ab022f6
adityrajtiwary <122718170+adityrajtiwary@users.noreply.github.com>
2024-06-22 00:46:50 +0530

    Add files via upload

---
+++ lecture1.cpp
@@ -0,0 +1,9 @@
+//we learn about binary codes 
+//example:- 
+//0 = 0000 in binary
+//1 = 0001 in binary
+//2 = 0010 in binary 
+
+// Learn about how we compile the code
+// we learn about what is compiler and how compiler takes our human readable words and compile the code to machine level to understand and execute it properly 
+
